b184e1ff36c8d3fccc71a6310bd16498bd481940
tabassumsabaa <sabihatabassum0511@gmail.com>
2024-08-30 16:33:47 +0530

    get it of surveys

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { ObjectId } = require('mongodb');
+
 
 
 //middleware
@@ -26,12 +28,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const surveyCollection = client.db("survey").collection("suurveys");
+
+    //Apis
+    app.get("/suurveys", async(req, res) =>{
+        const result = await surveyCollection.find().toArray();
+        res.send(result);
+    });
+    
+    app.get("/suurveys/:id", async(req, res) =>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await surveyCollection.findOne(query);
+        res.send(result);
+    });
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
