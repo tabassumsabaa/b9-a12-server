9cdeef0b2f383e2f7d5ea4d86989a9d81ba0aafd
tabassumsabaa <sabihatabassum0511@gmail.com>
2024-08-31 20:52:59 +0530

    user deleted

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
             }  
             const result = await userCollection.insertOne(user);
             res.send(result);
+        });
+        app.delete("/users/:id", async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
         })
 
 
